5a2a26b67191b261b4408e20f5e2356e8e038c61
yanggy <yangguiyun250725@163.com>
2017-09-30 22:11:07 +0800

    edit

--- cloud-service-provider/src/main/java/com/yanggy/cloud/ServiceRegister.java
+++ cloud-service-provider/src/main/java/com/yanggy/cloud/ServiceRegister.java
@@ -3,11 +3,13 @@ package com.yanggy.cloud;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * Created by yangguiyun on 2017/6/12.
  */
 
+@EnableTransactionManagement
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceRegister {
