4dd526170be3fccc40f9ba5842fcfdc5f8f736e1
yangguiyun <yangguiyun@mobanker.com>
2017-10-13 15:14:09 +0800

    add userRole role files

--- cloud-entity/src/main/java/com/yanggy/cloud/entity/User.java
+++ cloud-entity/src/main/java/com/yanggy/cloud/entity/User.java
@@ -1,55 +1,20 @@
 package com.yanggy.cloud.entity;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * Created by yangguiyun on 2017/6/1.
  */
 
+@Data
 public class User implements Serializable {
     private long id;
     private String name;
     private String password;
     private int sex;
     private int age;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getSex() {
-        return sex;
-    }
-
-    public void setSex(int sex) {
-        this.sex = sex;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String email;
+    private  String phone;
 }
